420b7418d22bb3441fff476a2eaab4c954eddf2a
manojbaniya444 <86000102+manojbaniya444@users.noreply.github.com>
2023-10-07 13:29:31 +0545

    Initial server setup

---
+++ server/server.js
@@ -0,0 +1,8 @@
+const express = require("express");
+require("dotenv").config();
+
+const server = express();
+
+server.listen(process.env.PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
